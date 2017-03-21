590dc92b6619979fb1cbe5d7b1ddc0897237f511
itsMontoya <josh@montoya.io>
2017-03-21 10:57:24 -0700

    Adding debug statements

--- journaler.go
+++ journaler.go
@@ -7,50 +7,139 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"os"
+	"runtime"
 	"strings"
 )
 
 var (
-	successStr = color.New(color.FgGreen, color.Bold).Sprint("[Sucess]")
-	warningStr = color.New(color.FgYellow, color.Bold).Sprint("[Warning]")
-	errorStr   = color.New(color.FgRed, color.Bold).Sprint("[Error]")
+	successColor = color.New(color.FgGreen, color.Bold)
+	defaultColor = color.New(color.Bold)
+	warningColor = color.New(color.FgYellow, color.Bold)
+	errorColor   = color.New(color.FgRed, color.Bold)
 )
 
-const msgFmt = "%s %s\n"
+const (
+	msgFmt   = "%s %s\n"
+	debugFmt = "%s (%s:%d) %s\n"
+	labelFmt = "[%s]"
+)
 
-var j = Journal{
-	w: os.Stdout,
-}
+var j = NewJournal(os.Stdout)
 
 // New returns a new Journaler
 func New(prefixs ...string) *Journaler {
 	return j.New(prefixs...)
 }
 
+// SetLabel will set a label for the global Journal
+func SetLabel(key, value string) bool {
+	return j.SetLabel(key, value)
+}
+
+// NewJournal will return a new Journal
+// Note: This is only needed if you need to output somewhere other than os.Stdout
+func NewJournal(w io.Writer) *Journal {
+	var j Journal
+	j.w = w
+	j.SetLabel("success", "Success")
+	j.SetLabel("notification", "Notification")
+	j.SetLabel("warning", "Warning")
+	j.SetLabel("error", "Error")
+	j.SetLabel("debug", "Debug")
+	return &j
+}
+
+// Journal is the back-bone of the Journalers. Manages the writer and thread-safety
 type Journal struct {
 	mux sync.Mutex
 	// Output writer
 	w io.Writer
+
+	successStr      string
+	notificationStr string
+	warningStr      string
+	errorStr        string
+	debugStr        string
+}
+
+// SetLabel will set a label
+func (j *Journal) SetLabel(key, value string) (ok bool) {
+	ok = true
+	j.mux.Lock()
+
+	switch key {
+	case "success":
+		j.successStr = successColor.Sprintf(labelFmt, value)
+	case "notification":
+		j.notificationStr = defaultColor.Sprintf(labelFmt, value)
+	case "warning":
+		j.warningStr = warningColor.Sprintf(labelFmt, value)
+	case "error":
+		j.errorStr = errorColor.Sprintf(labelFmt, value)
+	case "debug":
+		j.debugStr = defaultColor.Sprintf(labelFmt, value)
+
+	default:
+		ok = false
+	}
+
+	j.mux.Unlock()
+	return
 }
 
 // Success is for success messages
 func (j *Journal) Success(msg string) {
 	j.mux.Lock()
-	j.w.Write([]byte(fmt.Sprintf(msgFmt, successStr, msg)))
+	fmt.Fprintf(j.w, msgFmt, j.successStr, msg)
+	j.mux.Unlock()
+}
+
+// Notification is for notification messages
+func (j *Journal) Notification(msg string) {
+	j.mux.Lock()
+	fmt.Fprintf(j.w, msgFmt, j.notificationStr, msg)
 	j.mux.Unlock()
 }
 
 // Warn is for warning messages
 func (j *Journal) Warn(msg string) {
 	j.mux.Lock()
-	j.w.Write([]byte(fmt.Sprintf(msgFmt, warningStr, msg)))
+	fmt.Fprintf(j.w, msgFmt, j.warningStr, msg)
 	j.mux.Unlock()
 }
 
 // Error is for error messages
 func (j *Journal) Error(msg string) {
 	j.mux.Lock()
-	j.w.Write([]byte(fmt.Sprintf(msgFmt, errorStr, msg)))
+	fmt.Fprintf(j.w, msgFmt, j.errorStr, msg)
+	j.mux.Unlock()
+}
+
+// Output is for custom messages
+func (j *Journal) Output(label, color, msg string) {
+	j.mux.Lock()
+
+	switch color {
+	case "green":
+		label = successColor.Sprintf(labelFmt, label)
+	case "yellow":
+		label = warningColor.Sprintf(labelFmt, label)
+	case "red":
+		label = errorColor.Sprintf(labelFmt, label)
+
+	default:
+		label = defaultColor.Sprintf(labelFmt, label)
+	}
+
+	fmt.Fprintf(j.w, msgFmt, label, msg)
+	j.mux.Unlock()
+}
+
+// Debug is for debug messages
+func (j *Journal) Debug(msg string) {
+	fn, ln := getDebugVals()
+	j.mux.Lock()
+	fmt.Fprintf(j.w, debugFmt, j.debugStr, fn, ln, msg)
 	j.mux.Unlock()
 }
 
@@ -62,6 +151,7 @@ func (j *Journal) New(prefixs ...string) *Journaler {
 	}
 }
 
+// Journaler is for logging with a prefix
 type Journaler struct {
 	j      *Journal
 	prefix string
@@ -72,6 +162,11 @@ func (j *Journaler) Success(msg string) {
 	j.j.Success(j.prefix + msg)
 }
 
+// Notification is for notification messages
+func (j *Journaler) Notification(msg string) {
+	j.j.Notification(j.prefix + msg)
+}
+
 // Warn is for warning messages
 func (j *Journaler) Warn(msg string) {
 	j.j.Warn(j.prefix + msg)
@@ -80,6 +175,16 @@ func (j *Journaler) Warn(msg string) {
 // Error is for error messages
 func (j *Journaler) Error(err error) {
 	j.j.Error(j.prefix + err.Error())
+}
+
+// Output is for custom messages
+func (j *Journaler) Output(label, color, msg string) {
+	j.j.Output(label, color, j.prefix+msg)
+}
+
+// Debug is for debug messages
+func (j *Journaler) Debug(msg string) {
+	j.j.Debug(j.prefix + msg)
 }
 
 func getShort(file string) string {
@@ -98,5 +203,11 @@ func getShort(file string) string {
 		}
 	}
 
-	return strings.Join(ns, "/")
+	return "/" + strings.Join(ns, "/")
+}
+
+func getDebugVals() (filename string, lineNumber int) {
+	_, filename, lineNumber, _ = runtime.Caller(3)
+	filename = getShort(filename)
+	return
 }
--- journaler_test.go
+++ journaler_test.go
@@ -8,6 +8,19 @@ import (
 func TestJournaler(t *testing.T) {
 	j := New("Main service", "Sub service")
 	j.Success("Database entry posted")
+	j.Notification("CPU temperatures are at 40*C")
 	j.Warn("Update to remote server has failed")
 	j.Error(errors.New("Danger, Will Robinson!"))
+	j.Debug("debug test")
+
+	// Testing custom labels
+	SetLabel("error", "uh oh.")
+	j.Error(errors.New("Danger, Will Robinson!"))
+
+	// Testing custom output
+	j.Output("Compliment", "green", "You smell nice.")
+	j.Output("System", "default", "Server will be rebooting for maintenance in 45 minutes")
+	j.Output("System", "yellow", "Server will be rebooting for maintenance in 5 minutes")
+	j.Output("System", "red", "Server will be rebooting for maintenance in 5 seconds")
+
 }
